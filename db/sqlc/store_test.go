d1b9412987bcd2f1e0e7009b9ed857b0fd7b7909
rendisibarani94 <rendisibarani94@gmail.com>
2025-11-25 02:24:51 +0700

    test: adding TestTransferTxDeadlock unit test, for deadlock scenario

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -109,3 +109,51 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 }
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+	n := 10
+	amount := int64(10)
+
+	// channels to receive results and errors
+	errch := make(chan error)
+	// resultch := make(chan TransferTxResult)
+
+	// example: run n concurrent transfer transactions
+	for i := 0; i < n; i++ {
+		FromAccountID := account1.ID
+		ToAccountID := account2.ID
+		if i%2 == 1 {
+			FromAccountID = account2.ID
+			ToAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: ToAccountID,
+				ToAccountID:   FromAccountID,
+				Amount:        amount,
+			})
+			errch <- err
+			// resultch <- result
+		}()
+	}
+	for range n {
+		err := <-errch
+		require.NoError(t, err)
+	}
+
+	// check final updated balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
