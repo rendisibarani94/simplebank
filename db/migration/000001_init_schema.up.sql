77a38f1e43643e5b2847d110cc262591112741e5
rendisibarani94 <rendisibarani94@gmail.com>
2025-11-24 12:15:09 +0700

    fix: update mismatch entries id in schema migration, and fixing getEntry Query

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE "accounts" (
 );
 
 CREATE TABLE "entries" (
-  "id" int PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
--- db/query/entry.sql
+++ db/query/entry.sql
@@ -5,7 +5,7 @@ RETURNING *;
 
 -- name: GetEntry :one
 SELECT * FROM entries
-WHERE id = $1;
+WHERE id = $1 LIMIT 1;
 
 -- name: ListEntries :many
 SELECT * FROM entries
